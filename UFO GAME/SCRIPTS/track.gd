df533050b584977d91200dee97ac360e2a846898
ChyRoseBun <chyrose1927@gmail.com>
2025-02-05 16:55:40 -0500

    AddNewCyanMaze

--- UFO GAME/SCRIPTS/track.gd
+++ UFO GAME/SCRIPTS/track.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+var body_array := []
 
 func _on_area_2d_body_exited(body):
-	get_tree().reload_current_scene()
+	if body in body_array:
+		get_tree().reload_current_scene()
+
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if not body in body_array:
+		body_array.append(body)
