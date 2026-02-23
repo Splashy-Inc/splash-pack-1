012803ee8154ca1e59f1a46b6b02602a245650e6
avelitj12 <avelitj12@gmail.com>
2026-02-23 08:00:14 -0500

    Add TVs and game images to main menu

--- SCRIPTS/game_button.gd
+++ SCRIPTS/game_button.gd
@@ -3,6 +3,10 @@ extends Button
 class_name GameButton
 
 @export var game_scene : PackedScene
+@export var game_texture : Texture2D
+
+func _ready() -> void:
+	$GameTexture.texture = game_texture
 
 func _on_pressed() -> void:
 	Globals.game_selected.emit(game_scene)
